34020de97a4dc6d9c5a074342249fe4337d68e00
Jingchang Shi <jingchangshi@gmail.com>
2023-03-19 21:06:27 +0800

    102

---
+++ 102.cpp
@@ -0,0 +1,91 @@
+#include <vector>
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+
+    TreeNode* buildTreeFromArray(vector<int>& inorder, vector<int>& postorder,
+              int &root_idx, const int in_beg, const int in_end) {
+        if (in_end - in_beg == 0) {
+          return nullptr;
+        }
+        TreeNode *root = new TreeNode();
+        int root_val = postorder[root_idx];
+        root->val = root_val;
+        root_idx--;
+        if (in_end - in_beg == 1) {
+          // Only 1 element exist. It is the root of this level.
+          return root;
+        }
+        auto root_it = find(inorder.begin() + in_beg, inorder.begin() + in_end, root_val);
+        // int left_end = root_it - &inorder[in_beg];
+        int left_end = root_it - inorder.begin();
+        int right_beg = left_end + 1;
+        root->right = buildTreeFromArray(inorder, postorder, root_idx, right_beg, in_end);
+        root->left = buildTreeFromArray(inorder, postorder, root_idx, in_beg, left_end);
+        return root;
+    }
+
+    // The post order array lists the root node at each level.
+    // The we can use the root node to split the in order array
+    // to obtain the left and right child of the root node.
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+      int n = postorder.size();
+      int root_idx = n - 1;
+      TreeNode *root = buildTreeFromArray(inorder, postorder, root_idx, 0, n);
+      return root;
+    }
+
+    void dfsInorder(TreeNode *root, vector<vector<int>>& vec, int m) {
+      if (root == nullptr) {
+        return;
+      }
+      dfsInorder(root->left, vec, m+1);
+      if (vec.size() <= m) {
+        vec.resize(m+1);
+        vec[m].push_back(root->val);
+      } else {
+        vec[m].push_back(root->val);
+      }
+      dfsInorder(root->right, vec, m+1);
+    }
+
+    vector<vector<int>> levelOrder(TreeNode* root) {
+      vector<vector<int>> vec;
+      dfsInorder(root, vec, 0);
+      return vec;
+    }
+};
+
+int main(int argc, char **argv) {
+  vector<int> inorder = {9,3,15,20,7};
+  vector<int> postorder = {9,15,7,20,3};
+  Solution sol;
+  TreeNode* root = sol.buildTree(inorder, postorder);
+  vector<vector<int>> res = sol.levelOrder(root);
+  int n = res.size();
+  for (int i = 0; i < n; i++) {
+    int m = res[i].size();
+    for (int j = 0; j < m-1; j++) {
+      printf("%d,", res[i][j]);
+    }
+    printf("%d\n", res[i][m-1]);
+  }
+  return 0;
+}
